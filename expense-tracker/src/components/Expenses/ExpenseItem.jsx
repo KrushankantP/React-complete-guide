5347d7c21df27c013495d027a0eb88f1a9a37f0a
Krushankant  Patel <krpatel@virtusa.com>
2022-03-24 12:04:01 -0500

    listing to Events & working with Event Handlers

--- expense-tracker/src/components/Expenses/ExpenseItem.jsx
+++ expense-tracker/src/components/Expenses/ExpenseItem.jsx
@@ -4,7 +4,10 @@ import './ExpenseItem.css';
 import Card from '../UI/Card'
 
 const ExpenseItem = (props) => {
-
+ 
+  const clickHandler =() =>{
+    console.log('Clicked !!!')
+  }
     return (
     <Card className ='expense-item'>
           <ExpenseDate date={props.date} />
@@ -12,6 +15,7 @@ const ExpenseItem = (props) => {
             <h2>{props.title}</h2>
             <div className="expense-item__price">${props.amount}</div>
           </div>
+          <button onClick={clickHandler}>change Title</button>
     </Card>
       );
     }
