75bbce3420c432c01f45b3e88ab606ee71d77b62
Krushankant  Patel <krpatel@virtusa.com>
2022-03-25 00:09:11 -0500

    Adding Form Inputs

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => {
   const expenses = [
@@ -31,8 +32,8 @@ const App = () => {
   // );
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <Expenses items={expenses} />
+      <NewExpense />
+          <Expenses items={expenses} />
     </div>
   );
 }
