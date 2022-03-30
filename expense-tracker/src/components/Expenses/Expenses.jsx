9ee6dbdeae237e82497768a243e0a2ae13703998
Krushankant  Patel <krpatel@virtusa.com>
2022-03-29 22:38:36 -0500

    Rendaring Lists of Data

--- expense-tracker/src/components/Expenses/Expenses.jsx
+++ expense-tracker/src/components/Expenses/Expenses.jsx
@@ -1,11 +1,10 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 
-const Expenses = (props) => {
-
+const Expenses = props => {
   const [filteredYear, setFilteredYear] = useState('2020');
 
   const filterChangeHandler = selectedYear => {
@@ -14,31 +13,21 @@ const Expenses = (props) => {
 
   return (
     <div>
-      <Card className="expenses">
-          <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+      <Card className='expenses'>
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {props.items.map(expense => (
           <ExpenseItem
-            title={props.items[0].title}
-            amount={props.items[0].amount}
-            date={props.items[0].date}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
           />
-          <ExpenseItem
-            title={props.items[1].title}
-            amount={props.items[1].amount}
-            date={props.items[1].date}
-          />
-          <ExpenseItem
-            title={props.items[2].title}
-            amount={props.items[2].amount}
-            date={props.items[2].date}
-          />
-          <ExpenseItem
-            title={props.items[3].title}
-            amount={props.items[3].amount}
-            date={props.items[3].date}
-          />
+        ))}
       </Card>
     </div>
   );
-}
+};
 
 export default Expenses;
