d196f811bf19bd7025c473d9cafac84b86dde22d
Krushankant  Patel <krpatel@virtusa.com>
2022-04-05 18:39:06 -0500

    Ass-4  add expense form visible on button click

--- expense-tracker/src/components/NewExpense/ExpenseForm.jsx
+++ expense-tracker/src/components/NewExpense/ExpenseForm.jsx
@@ -72,7 +72,9 @@ return(
     </div>
 </div>
 <div className="new-expense__actions">
+    <button type="button" onClick={props.onCancel}>Cancel</button>
     <button type="submit">Add Expense</button>
+
 </div>
 </form>
 );
