904a71766b39d738941174657c7cfbdc1e1b121e
Krushankant  Patel <krpatel@virtusa.com>
2022-05-05 13:53:37 -0500

    manging user input - fix and Adding validation & Resstting Loging.

--- 3.Practice-Project/src/components/Users/AddUser.jsx
+++ 3.Practice-Project/src/components/Users/AddUser.jsx
@@ -9,7 +9,15 @@ const AddUser = (props) =>{
 
     const addUserHandler =(event) => {
         event.preventDefault();
+        if(enteredUsername.trim().length === 0  || enteredAge.trim().length === 0){
+            return ;
+        }
+        if(+enteredAge < 1){
+
+        }
         console.log(enteredUsername, enteredAge);
+        setEnteredUsername('');
+        setEnteredAge('');
     }
     const usernameChangeHandler =(event) =>{
         setEnteredUsername(event.target.value);
@@ -23,9 +31,9 @@ const AddUser = (props) =>{
         <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username">Username</label>
-                <input type="text"  id ="username" onChange={usernameChangeHandler}/>
+                <input type="text"  id ="username" value={enteredUsername} onChange={usernameChangeHandler}/>
                 <label htmlFor="age">Age (Years)</label>
-                <input type="number"  id ="age"/>
+                <input type="number"  id ="age" value={enteredAge} onChange={ageChangeHandler}/>
                 <Button type="submit">Add User</Button>
             </form>
         </Card>
