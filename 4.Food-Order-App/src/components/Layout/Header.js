0ece67cf6e736ad7149474148b9d6801559ccbd8
Krushankant Patel <patelkk1618@gmail.com>
2022-07-08 00:48:31 -0500

    Managing Cart & Modal State

--- 4.Food-Order-App/src/components/Layout/Header.js
+++ 4.Food-Order-App/src/components/Layout/Header.js
@@ -5,15 +5,17 @@ import HeaderCartButton from './HeaderCartButton';
 
 const Header = props =>{
 
-    return <Fragment>
+    return (
+    <Fragment>
         <header className={classes.header}>
             <h1>React Meals</h1>
-            <HeaderCartButton/>
+            <HeaderCartButton onClick={props.onShowCart} />
         </header>
         <div className= {classes['main-image']}>
             <img src={mealsImage} alt='A table full of delicious food!'/>
         </div>
     </Fragment>
+    );
 };
 
 export default Header;
