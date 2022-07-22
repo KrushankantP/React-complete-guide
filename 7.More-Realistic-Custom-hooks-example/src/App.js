d59fd66d96e0e4f9ebb00d444abd372e25651cb2
Krushankant Patel <patelkk1618@gmail.com>
2022-07-22 17:38:58 -0500

    Adjusting the Custom hook logic

--- 7.More-Realistic-Custom-hooks-example/src/App.js
+++ 7.More-Realistic-Custom-hooks-example/src/App.js
@@ -7,23 +7,23 @@ import useHttp from './hooks/use-http';
 function App() {
 
   const [tasks, setTasks] = useState([]);
-  const transformTasks = taskObj => {
-    const loadedTasks = [];
-
-    for (const taskKey in taskObj) {
-      loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
-    }
-  setTasks(loadedTasks);
-  };
 
 
- const {isLoading, error, sendRequest: fetchTasks } = useHttp(
-  {url: 'https://custom-hook-91a94-default-rtdb.firebaseio.com/tasks.json'},
-   transformTasks);
+
+ const {isLoading, error, sendRequest: fetchTasks } = useHttp();
 
   useEffect(() => {
-    fetchTasks();
-  }, []);
+    const transformTasks = ((taskObj) => {
+      const loadedTasks = [];
+  
+      for (const taskKey in taskObj) {
+        loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
+      }
+    setTasks(loadedTasks);
+    });
+
+    fetchTasks({url: 'https://custom-hook-91a94-default-rtdb.firebaseio.com/tasks.json'}, transformTasks);
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
